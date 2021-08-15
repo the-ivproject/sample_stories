cfd2aa712807a85b34131e24a2633aa1d92de3e5
ivannalis <71641625+ivannalis@users.noreply.github.com>
2021-08-15 19:12:03 +0700

    New Feature

--- js/new.js
+++ js/new.js
@@ -24,6 +24,8 @@ $(document).ready(
                 let id = inpt[i].id.replace('genInput','')
                 getGPSbyStoryID(id)
             }
+
+           
         })
     });
 
@@ -133,66 +135,6 @@ function getGPSbyStoryID(story_id) {
 
 }
 
-function getGPSbyStoryID2(story_id) {
-
-    // let point = document.querySelectorAll('leaflet-marker-icon')
-    // point.forEach(a => {
-    //     a.style.visibility = 'hidden'
-    // })
-    // console.log(point)
-    parameter = {
-        url: sheetsUrl,
-        //command:"getLandmarksByStory",
-        command: "getLandmarksByStory",
-        story_id: story_id
-    };
-    $.get(appUrl, parameter, function (data) {
-        //console.log(data);
-        var data_json = JSON.parse(data);
-
-        var gps_locations = [];
-        for (i in data_json.table) {
-            gps_locations.push({
-                lat: data_json.table[i].lat,
-                lng: data_json.table[i].lng,
-                name: data_json.table[i].name,
-                content: data_json.table[i].content,
-                link: data_json.table[i].link,
-                landmark_id: data_json.table[i].landmark_id,
-            })
-
-
-        }
-   
-        refreshMap2(gps_locations, story_id);
-
-        let markerIcon = document.querySelectorAll('.leaflet-marker-icon')
-        let markerShadow = document.querySelectorAll('.leaflet-marker-shadow')
-
-        if (gps_locations.length != markerIcon.length) {
-            for (let i = 0; i < (markerShadow.length - gps_locations.length); i++) {
-                markerIcon[i].remove()
-                markerShadow[i].remove()
-            }
-        }
-        let main_input = document.querySelectorAll('.groupinput').checked = true
-
-        // document.getElementById(`collapse_${story_id}`).classList.add("show");
-       
-        let genInpt = document.getElementById(`genInput${story_id}`)
-        genInpt.checked = true
-        MultiCheck(story_id,genInpt.checked)
-        
-    });
-    /*[{
-            lat: 25.0682519,
-            lng: 121.5922095
-        },
-    ]*/
-
-
-}
-
 function ZoomByStoryID(story_id) {
     console.log(story_id);
     // let point = document.querySelectorAll('leaflet-marker-icon')
@@ -218,28 +160,13 @@ function ZoomByStoryID(story_id) {
                 lng: data_json.table[i].lng,
 
             })
+
         }
-        
         ZoomToGroup(gps_locations)
         let genInpt = document.getElementById(`genInput${story_id}`)
         genInpt.checked = true
-
         MultiCheck(story_id,genInpt.checked)
-
-        let markerIcon = document.querySelectorAll('.leaflet-marker-icon')
-        let markerShadow = document.querySelectorAll('.leaflet-marker-shadow')
-        console.log('markerIcon',markerIcon)
-        if (markerIcon.length === 0) {
-            GetCluster(story_id)
-        } else {
-            console.log('no iconssss',gps_locations.length)
-            console.log('no icon',markerIcon.length)
-            for (let i = 0; i < (markerShadow.length - gps_locations.length); i++) {
-                markerIcon[i].remove()
-                markerShadow[i].remove()
-            }
-        }
-
+        GetCluster(story_id)
     });
     /*[{
             lat: 25.0682519,
@@ -249,8 +176,6 @@ function ZoomByStoryID(story_id) {
 
 
 }
-
-
 // parameter = {
 //     url: sheetsUrl,
 //     //command:"getLandmarksByStory",
@@ -288,7 +213,7 @@ function addmyappList(div_id_to_add, data_to_append, where_to_add, id_div) {
     html_reg += '     <h2 class=\"accordion-header\" id=\"heading_' + myapp_story_id + '\" style="padding:10px;font-size:16px">';
     html_reg += '       <button style="width:50px;float:right;height:100%;padding:0;background:white;box-shadow:none" class=\"accordion-button\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#collapse_' + myapp_story_id + '\" aria-expanded=\"true\" aria-controls=\"collapse_' + myapp_story_id + '\">';
     html_reg += '       </button>';
-    html_reg += '           <input id=\"genInput' + myapp_story_id + '\" class="groupinput" type=\"checkbox\"> (type:' + myapp_types + ') <a style="color:#0d6efd;text-decoration:underline;cursor:pointer" onclick=\"javascript:getGPSbyStoryID2(' + myapp_story_id + ')\">' + myapp_title + '</a> <a href=\"javascript:spec_func(' + myapp_story_id + ')\">(add)</a>';
+    html_reg += '           <input id=\"genInput' + myapp_story_id + '\" class="groupinput" type=\"checkbox\"> (type:' + myapp_types + ') <a style="color:#0d6efd;text-decoration:underline;cursor:pointer" onclick=\"javascript:ZoomByStoryID(' + myapp_story_id + ')\">' + myapp_title + '</a> <a href=\"javascript:spec_func(' + myapp_story_id + ')\">(add)</a>';
     html_reg += '     </h2>';
     html_reg += '     <div id=\"collapse_' + myapp_story_id + '\" class=\"accordion-collapse collapse\" aria-labelledby=\"heading_' + myapp_story_id + '\" data-bs-parent=\"#accordionExample\">';
     html_reg += '       <div class=\"accordion-body\">';
@@ -357,11 +282,34 @@ function addmyappList(div_id_to_add, data_to_append, where_to_add, id_div) {
 }
 
 
-function addStoriesToLayer(locations) {
+function addStoriesToLayer(id,locations,val) {
     var markers = L.markerClusterGroup();
     locations.map(item => L.marker(new L.LatLng(item.lat, item.lng)))
         .forEach(item => markers.addLayer(item));
-    mymap.addLayer(markers)
+    
+
+  
+        // mymap.addLayer(markers)
+   
+    switch (val) {
+        case true:
+            mymap.eachLayer(function (layer) {
+                mymap.addLayer(layer)
+            })
+            mymap.addLayer(markers);
+            MultiCheck(id,val)
+            console.log(val)
+            break;
+        case false:
+            markers.eachLayer(function (layer) {
+                layer.remove()
+            })
+            mymap.removeLayer(markers);
+            MultiCheck(id,val)
+            console.log(val)
+            break;
+        }
+   
 }
 
 // function ZoomToGroup(input, coor) {
@@ -414,20 +362,20 @@ function ZoomToGroup(coor) {
     // ZoomToGroup(markers)
 }
 
-function ShowHideCluster(location,input) {
-    var markers = L.markerClusterGroup();
-    input.addEventListener('click', () => {
-        if (input.checked === false) {
-            markers.removeLayer(location)
-        } else {
-            markers.addLayer(location)
-        }
-    })
+// function ShowHideCluster(location,input) {
+//     var markers = L.markerClusterGroup();
+//     input.addEventListener('click', () => {
+//         if (input.checked === false) {
+//             markers.removeLayer(location)
+//         } else {
+//             markers.addLayer(location)
+//         }
+//     })
 
-    mymap.addLayer(markers)
+//     mymap.addLayer(markers)
 
   
-}
+// }
 
 function onclickTitleShowMarker(location) {
     var markers = L.markerClusterGroup();
@@ -459,12 +407,16 @@ function GetCluster(story_id) {
                 landmark_id: data_json.table[i].landmark_id,
             })
         }
+        let x = document.getElementById(`genInput${story_id}`)
         
+        addStoriesToLayer(story_id,gps_locations,true)
+            x.addEventListener('click',function(){
+                addStoriesToLayer(story_id,gps_locations,x.checked)
+            })
+       
 
+       
 
-        
-        // onclickTitleShowMarker(gps_locations)
-        addStoriesToLayer(gps_locations)
     });
 }
 
@@ -474,6 +426,7 @@ function MultiCheck(id,val) {
         child.checked = val
     })
 }
+
 function refreshMap(locations, sid) {
 
     // if(markerIcon.length !== locations.length) {
@@ -534,27 +487,39 @@ mymap = L.map('map', {
     //         markerShadow[i].remove()
     //     }
     // }
-    let markerIcon = document.querySelectorAll('.leaflet-marker-icon')
-    let markerShadow = document.querySelectorAll('.leaflet-marker-shadow')
     genInput.addEventListener('click', function () {
         let id = genInput.id.replace('genInput','')
         let val = genInput.checked
-        
-        if (genInput.checked === true) {
-            mymap.eachLayer(function (layer) {
-                mymap.addLayer(layer)
-            })
-            mymap.addLayer(markers);
-            MultiCheck(id,val)
-            console.log(markerIcon.length)
-        } else {
-            markers.eachLayer(function (layer) {
-                layer.remove()
-            })
-            mymap.removeLayer(markers);
-            MultiCheck(id,val)
-            console.log(markerIcon.length)
-        }
+        switch (genInput.checked) {
+            case true:
+                mymap.eachLayer(function (layer) {
+                    mymap.addLayer(layer)
+                })
+                mymap.addLayer(markers);
+                MultiCheck(id,val)
+              break;
+            case false:
+                markers.eachLayer(function (layer) {
+                    layer.remove()
+                })
+                mymap.removeLayer(markers);
+                MultiCheck(id,val)
+              break;
+          }
+        // if (genInput.checked === true) {
+        //     mymap.eachLayer(function (layer) {
+        //         mymap.addLayer(layer)
+        //     })
+        //     mymap.addLayer(markers);
+        //     MultiCheck(id,val)
+            
+        // } else if(genInput.checked === false) {
+        //     markers.eachLayer(function (layer) {
+        //         layer.remove()
+        //     })
+        //     mymap.removeLayer(markers);
+        //     MultiCheck(id,val)
+        // }
     })
 
     // ZoomToGroup(markers)
@@ -593,212 +558,6 @@ mymap = L.map('map', {
     // toDel.forEach(n => markers.removeLayer(n));
 
     mymap.removeLayer(markers);
-
-    //setTimeout(function(){
-    // markers.zoomToBounds({padding: [20, 20]});
-    //mymap.rem
-    //mymap.removeControl(p_control);
-    //p_control.removeFrom(mymap);
-    //p_control = L.control.layers(baseMaps, overlayMaps).addTo(mymap);
-    /*
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox/streets-v11',
-  tileSize: 512,
-  zoomOffset: -1,
-  accessToken: 'pk.eyJ1IjoiaW9rc2VuZ3RhbiIsImEiOiJja3JkeTgxMHI1Z3B2MzFxcHM0NWo3cTEwIn0.kkcIlaMdiTpqqaCk6YpOgQ'
-}).addTo(mymap);
-*/
-    /*mymap.setLayoutProperty('country-label', 'text-field', [
-    'get',
-    'name_zh-Hant'
-    ]);
-    */
-    /*
-    var markers = L.markerClusterGroup();
-
-    locations.map(item => L.marker(new L.LatLng(item.lat, item.lng)))
-               //.forEach(item => mymap.addLayer(item));
-               .forEach(item => markers.addLayer(item));
-    mymap.addLayer(markers);
-    */
-    /*
-      map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 8,
-          center: {
-              lat: 24.790078397806973,
-              lng: 121.07471724480152
-          },
-      });
-      const infowindow = new google.maps.InfoWindow({
-        content: "<h1>test</h1>",
-      });
-      // Create an array of alphabetical characters used to label the markers.
-      let labels = [];
-      for (location_id in locations){
-        //const labels = ["", "B1"];
-        labels.push(locations[location_id].name);
-      }
-      // Add some markers to the map.
-      // Note: The code uses the JavaScript Array.prototype.map() method to
-      // create an array of markers based on a given "locations" array.
-      // The map() method here has nothing to do with the Google Maps API.
-      markers = locations.map((location, i) => {
-          var marker = new google.maps.Marker({
-              position: location,
-              label: labels[i % labels.length],
-              title: 'test'
-          });
-          marker.addListener("click", () => {
-              //map.setZoom(8);
-              //map.setCenter(marker.getPosition());
-              infowindow.setContent(location.content + '<br/>' + '<a href=\"' + location.link + '\">link</a>');
-              infowindow.open(map, marker);
-              console.log(marker.getLabel());
-
-          });
-          return marker
-      });
-      // Add a marker clusterer to manage the markers.
-      markerCluster = new MarkerClusterer(map, markers, {
-          imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-      });
-      marker = new google.maps.Marker({
-        position: {lat:25.0489782,lng:121.5208181},
-      });
-      //markerCluster.addMarkers(marker, true);
-
-      google.maps.event.addListener(markerCluster, 'clusterclick', function(c) {
-          console.log('Number of managed markers in cluster: ' + c.getSize());
-          var m = c.getMarkers();
-          for (let i in m) {
-              //console.log(m[i].getLabel());
-              //console.log(m[i].getTitle());
-              //console.log(m[i].myObj.myKey);
-          }
-      });
-      */
-}
-function refreshMap2(locations, sid) {
-
-    // if(markerIcon.length !== locations.length) {
-    //     let test = markerIcon[0]
-    //     console.log(markerIcon)
-    // }
-
-    //mymap.invalidateSize();
-    //var mymap = L.map('map').setView([25.1130643, 121.5227629], 7);
-    console.log('refreshMap');
-    /*
-
-  var markers = L.markerClusterGroup();
-  //var landmarks_layergroup = L.layerGroup();
-  locations.map(item => L.marker(new L.LatLng(item.lat, item.lng)))
-             //.forEach(item => mymap.addLayer(item));
-             .forEach(item => markers.addLayer(item));
-
-mymap = L.map('map', {
-  center: [25.1130643, 121.5227629],
-  zoom: 7,
-  layers: [streets, markers]
-});*/
-
-
-    var streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: 'pk.eyJ1IjoiaW9rc2VuZ3RhbiIsImEiOiJja3JkeTgxMHI1Z3B2MzFxcHM0NWo3cTEwIn0.kkcIlaMdiTpqqaCk6YpOgQ'
-    });
-    
-    var markers = L.markerClusterGroup();
-    //var landmarks_layergroup = L.layerGroup();
-
-
-    let input = document.querySelectorAll(`#collapse_${sid} input`)
-    let a = document.querySelectorAll(`#collapse_${sid} .singleZoom`)
-
-    locations.map(item => L.marker(new L.LatLng(item.lat, item.lng)))
-        //.forEach(item => mymap.addLayer(item));
-        .forEach((item, i) => {
-            markers.addLayer(item)
-            ShowHideMarker(input[i], item,markers)
-            SingleZoom(a[i], item)
-     
-        });
-
-    let genInput = document.getElementById(`genInput${sid}`)
-    
-    // let markerShadow = document.querySelectorAll('.leaflet-marker-shadow')
-
-    // if (gps_locations.length != markerIcon.length) {
-    //     for (let i = 0; i < (markerShadow.length - gps_locations.length); i++) {
-    //         markerIcon[i].remove()
-    //         markerShadow[i].remove()
-    //     }
-    // }
-    let markerIcon = document.querySelectorAll('.leaflet-marker-icon')
-    let markerShadow = document.querySelectorAll('.leaflet-marker-shadow')
-    genInput.addEventListener('click', function () {
-        let id = genInput.id.replace('genInput','')
-        let val = genInput.checked
-        
-        if (genInput.checked === true) {
-            mymap.eachLayer(function (layer) {
-                mymap.addLayer(layer)
-            })
-            mymap.addLayer(markers);
-            MultiCheck(id,val)
-            console.log(markerIcon.length)
-        } else {
-            markers.eachLayer(function (layer) {
-                layer.remove()
-            })
-            mymap.removeLayer(markers);
-            MultiCheck(id,val)
-            console.log(markerIcon.length)
-        }
-    })
-
-    ZoomToGroup(locations)
-
-
-    var baseMaps = {
-        //    "Streets": streets
-    };
-
-    var overlayMaps = {
-        "Landmarks": markers
-    };
-    /*
-    mymap.eachLayer(function (layer) {
-        mymap.removeLayer(layer);
-    	//console.log(layer);
-    });
-    */
-    //mymap.clearLayers();
-
-
-
-    // const DIFF = +document.querySelector('input').value.replace(',', '.');
-    //     const toDel = [];
-
-    //     markers.getLayers().forEach((n, i, arr) => {
-    //         const coordN = n.getLatLng();
-    //         if (n !== arr.find(m => {
-    //         const coordM = m.getLatLng();
-    //         return !toDel.includes(m) && Object.keys(coordM).every(c => Math.abs(coordM[c] - coordN[c]) < DIFF);
-    //         })) {
-    //         toDel.push(n);
-    //         }
-    //     });
-
-    // toDel.forEach(n => markers.removeLayer(n));
-
-    mymap.addLayer(markers);
 
     //setTimeout(function(){
     // markers.zoomToBounds({padding: [20, 20]});
