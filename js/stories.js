48eaa6b268afea02523d9748cb702921cf0ea01b
ivannalis <71641625+ivannalis@users.noreply.github.com>
2021-08-15 07:38:28 +0700

    change js lib

--- js/stories.js
+++ js/stories.js
@@ -19,15 +19,7 @@ $(document).ready(
             for (i in data_json.table) {
                 addmyappList(gpsstory_list_all, data_json.table[i], 'prepend')
             }
-            //addMarker({lat:25.0489782,lng:121.5208181});
-           let x = document.querySelectorAll('.groupinput')
-           
-           for(let i = 0; i < x.length; i++){
-            GetCluster(x[i].id.replace("genInput",""))
- 
-               
-          
-           }
+        
            
         })
     });
@@ -212,7 +204,7 @@ function addmyappList(div_id_to_add, data_to_append, where_to_add, id_div) {
     html_reg += '     <h2 class=\"accordion-header\" id=\"heading_' + myapp_story_id + '\" style="padding:10px;font-size:16px">';
     html_reg += '       <button style="width:50px;float:right;height:100%;padding:0;background:white;box-shadow:none" class=\"accordion-button\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#collapse_' + myapp_story_id + '\" aria-expanded=\"true\" aria-controls=\"collapse_' + myapp_story_id + '\">';
     html_reg += '       </button>';
-    html_reg += '           <input id=\"genInput' + myapp_story_id + '\" class="groupinput" type=\"checkbox\"> (type:' + myapp_types + ') <a style="color:#0d6efd;text-decoration:underline;cursor:pointer" onclick=\"javascript:ZoomByStoryID(' + myapp_story_id + ')\">' + myapp_title + '</a> <a href=\"javascript:spec_func(' + myapp_story_id + ')\">(add)</a>';
+    html_reg += '           <input onclick=\"javascript:getGPSbyStoryID(' + myapp_story_id + ')\" id=\"genInput' + myapp_story_id + '\" class="groupinput" type=\"checkbox\"> (type:' + myapp_types + ') <a style="color:#0d6efd;text-decoration:underline;cursor:pointer" onclick=\"javascript:ZoomByStoryID(' + myapp_story_id + ')\">' + myapp_title + '</a> <a href=\"javascript:spec_func(' + myapp_story_id + ')\">(add)</a>';
     html_reg += '     </h2>';
     html_reg += '     <div id=\"collapse_' + myapp_story_id + '\" class=\"accordion-collapse collapse\" aria-labelledby=\"heading_' + myapp_story_id + '\" data-bs-parent=\"#accordionExample\">';
     html_reg += '       <div class=\"accordion-body\">';
